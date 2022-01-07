f7295b461c560f02ceab267bcded0bb8bb736b9a
alexo <alex-ong>
2022-01-07 11:11:16 +1100

    new: Add first test (always passes, its a manual check)

--- test/test_preview.py
+++ test/test_preview.py
@@ -7,23 +7,27 @@ import pytest
 import cv2
 import time
 import glob
-from calibrate.blockmatch import find_piece, BLOCKMATCH_SCALE_FACTOR
+from calibrate.blockmatch import find_piece, draw_and_show, convert_to_grayscale, BLOCKMATCH_SCALE_FACTOR
 
 
-
+# run using:
+# python -m pytest -s
 def test_previews():
     t = time.time()
     for imagePath in glob.glob("test/image/preview/*.png"):
-        print (f"testing {imagePath}")
+        print(f"testing {imagePath}")
         # load the image, convert it to grayscale, and initialize the
         # bookkeeping variable to keep track of the matched region
-        image = cv2.imread(imagePath,0)
-        dims = list(image.shape)        
-        #swap xy lol, opencv ftw
-        dims[1], dims[0] = (int(dims[0] * (BLOCKMATCH_SCALE_FACTOR / 2.0)),
-                           int(dims[1] * (BLOCKMATCH_SCALE_FACTOR / 2.0)))
-        
+        image = cv2.imread(imagePath, 0)
+        dims = list(image.shape)
+        # swap xy lol, opencv ftw
+        dims[1], dims[0] = (
+            int(dims[0] * (BLOCKMATCH_SCALE_FACTOR / 2.0)),
+            int(dims[1] * (BLOCKMATCH_SCALE_FACTOR / 2.0)),
+        )
+
         image = cv2.resize(image, dims)
         result = find_piece(image)
-        print (imagePath, result)
-    print ("Time:", time.time() - t)
+        image_color = cv2.cvtColor(image, cv2.COLOR_GRAY2BGR)
+        draw_and_show(image_color, result)
+    print("Time:", time.time() - t)
